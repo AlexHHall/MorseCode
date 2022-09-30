2898506c9c15754c57a7ac8d02cab23ced1a976c
Galaxcito <110064411+Galaxcito@users.noreply.github.com>
2022-09-30 20:42:44 +1300

    Update settings.py

--- settings.py
+++ settings.py
@@ -24,6 +24,8 @@ english_to_morse_list = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '
 
 
 def english_to_morse(english_text):
+    if not validate_eng(english_text)[0]:
+        return 'Invalid Character "' + validate_eng(english_text)[1] + '"'
     english_list = [i for i in english_text]
     for k, v in english_to_morse_list.items():
         for i in range(len(english_list)):
@@ -33,12 +35,32 @@ def english_to_morse(english_text):
 
 
 def morse_to_english(morse_code):
+    if not validate_morse(morse_code)[0]:
+        return 'Invalid Character "' + str(validate_morse(morse_code)[1]) + '"'
     morse_chars = morse_code.split(" ")
     for k, v in english_to_morse_list.items():
         for i in range(len(morse_chars)):
             if morse_chars[i] == v:
                 morse_chars[i] = k
     return "".join(i for i in morse_chars)
+
+def validate_eng(to_validate):
+    if to_validate == "":
+        return [1, False]
+    for char in to_validate.upper():
+        if not char in [k for k, v in english_to_morse_list.items()]:
+            return [0, char]
+    return [1, False]
+
+def validate_morse(to_validate):
+    if to_validate == "" or to_validate == " ":
+        return [1, False]
+    for char in to_validate.split(" "):
+        if not char.strip() in [v for k, v in english_to_morse_list.items()]:
+            if char.strip() == "":
+                return [1, False]
+            return [0, char]
+    return [1, False]
 
 
 english = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
